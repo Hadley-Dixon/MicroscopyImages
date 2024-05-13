05ebc637f4a7a495c9e61d833831f4fd25af62df
Hadley Dixon <hadley.dixon22@gmail.com>
2024-05-12 21:49:17 -0700

    update

--- FinalProject.py
+++ FinalProject.py
@@ -67,20 +67,39 @@ dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=16, shuffle
 class UNet(torch.nn.Module):
   def __init__(self):
     super().__init__()
-    self.conv1 = torch.nn.Conv2d(1, 64, kernel_size=3, padding='same')
-    self.conv2 = torch.nn.Conv2d(64, 64, kernel_size=3, padding='same')
-    self.conv3 = torch.nn.Conv2d(64, 128, kernel_size=3, padding='same')
-    self.conv4 = torch.nn.Conv2d(128, 128, kernel_size=3, padding='same')
-    self.conv5 = torch.nn.Conv2d(128, 256, kernel_size=3, padding='same')
-    self.conv6 = torch.nn.Conv2d(256, 256, kernel_size=3, padding='same')
-    self.conv7 = torch.nn.Conv2d(256, 512, kernel_size=3, padding='same')
-    self.conv8 = torch.nn.Conv2d(512, 512, kernel_size=3, padding='same')
-    self.conv9 = torch.nn.Conv2d(512, 1024, kernel_size=3, padding='same')
-    self.conv10 = torch.nn.Conv2d(1024, 1024, kernel_size=3, padding='same')
-    # self.dense1 = torch.nn.Linear(16*14*14, 10)
-
+    
     self.relu = torch.nn.ReLU()
     self.maxpool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
+    
+    # Encode
+    
+    self.conv1 = torch.nn.Conv2d(1, 64, kernel_size = 3, padding ='same')
+    self.conv2 = torch.nn.Conv2d(64, 64, kernel_size = 3, padding ='same')
+    self.conv3 = torch.nn.Conv2d(64, 128, kernel_size = 3, padding ='same')
+    self.conv4 = torch.nn.Conv2d(128, 128, kernel_size = 3, padding ='same')
+    self.conv5 = torch.nn.Conv2d(128, 256, kernel_size = 3, padding ='same')
+    self.conv6 = torch.nn.Conv2d(256, 256, kernel_size = 3, padding ='same')
+    self.conv7 = torch.nn.Conv2d(256, 512, kernel_size = 3, padding ='same')
+    self.conv8 = torch.nn.Conv2d(512, 512, kernel_size = 3, padding ='same')
+    self.conv9 = torch.nn.Conv2d(512, 1024, kernel_size = 3, padding='same')
+    self.conv10 = torch.nn.Conv2d(1024, 1024, kernel_size = 3, padding='same')
+    
+    # Decode
+
+    self.upConv1 = torch.nn.ConvTranspose2d(1024, 512, kernel_size = 2, stride=2, padding = 0)
+    self.upConv2 = torch.nn.ConvTranspose2d(512, 256, kernel_size = 2, stride=2, padding = 0)
+    self.upConv3 = torch.nn.ConvTranspose2d(256, 128, kernel_size = 2, stride=2, padding = 0)
+    self.upConv4 = torch.nn.ConvTranspose2d(128, 64, kernel_size = 2, stride=2, padding = 0)
+    
+    self.conv11 = torch.nn.Conv2d(1024, 512, kernel_size = 3, padding = 'same')
+    self.conv12 = torch.nn.Conv2d(512, 512, kernel_size = 3, padding = 'same')
+    self.conv13 = torch.nn.Conv2d(512, 256, kernel_size = 3, padding = 'same')
+    self.conv14 = torch.nn.Conv2d(256, 256, kernel_size = 3, padding = 'same')
+    self.conv15 = torch.nn.Conv2d(256, 128, kernel_size = 3, padding = 'same')
+    self.conv16 = torch.nn.Conv2d(128, 128, kernel_size = 3, padding = 'same')
+    self.conv17 = torch.nn.Conv2d(128, 64, kernel_size = 3, padding = 'same')
+    self.conv18 = torch.nn.Conv2d(64, 64, kernel_size = 3, padding = 'same')
+    self.conv19 = torch.nn.Conv2d(64, 1, kernel_size=3, padding='same')
 
   def forward(self, x):
 
@@ -102,7 +121,7 @@ class UNet(torch.nn.Module):
     
     copy2 = x
     
-    x = self.maxpool(x) # output (64, 32, 32)
+    x = self.maxpool(x) # output (128, 32, 32)
     
     x = self.conv5(x) # output (256, 32, 32)
     x = self.relu(x)
@@ -120,34 +139,58 @@ class UNet(torch.nn.Module):
     
     copy4 = x
     
-    x = self.maxpool(x) # output (256, 8, 8)
+    x = self.maxpool(x) # output (512, 8, 8)
     
-    # Base
-    
-    x = self.conv9(x) # output (1024, 8, 168
+    x = self.conv9(x) # output (1024, 8, 8)
     x = self.relu(x)
     x = self.conv10(x) # output (1024, 8, 8)
     x = self.relu(x)
     
     # Decode
     
-    # TODO
+    x = self.upConv1(x) # output (512, 16, 16)
+    x = torch.cat((copy4, x), dim = 1) # output (1024, 16, 16)
     
-    # # Next, a convolutional layer with 8 filters is applied. Then ReLU is applied.
-    # x = self.conv2(x) # output (8, 28, 28)
-    # x = self.relu(x)
+    x = self.conv11(x) # output (512, 16, 16)
+    x = self.relu(x)
+    x = self.conv12(x) # output (512, 16, 16)
+    x = self.relu(x)
+    
+    x = self.upConv2(x) # output (256, 32, 32)
+    x = torch.cat((copy3, x), dim = 1) # output (512, 32, 32)
+    
+    x = self.conv13(x) # output (256, 32, 32)
+    x = self.relu(x)
+    x = self.conv14(x) # output (256, 32, 32)
+    x = self.relu(x)
+    
+    x = self.upConv3(x) # output (128, 64, 64)
+    x = torch.cat((copy2, x), dim = 1) # output (256, 64, 64)
+    
+    x = self.conv15(x) # output (128, 64, 64)
+    x = self.relu(x)
+    x = self.conv16(x) # output (128, 64, 64)
+    x = self.relu(x)
+    
+    x = self.upConv4(x) # output (64, 128, 128)
+    x = torch.cat((copy1, x), dim = 1) # output (128, 128, 128)
+    
+    x = self.conv17(x) # output (64, 128, 128)
+    x = self.relu(x)
+    x = self.conv18(x) # output (64, 128, 128)
+    x = self.relu(x)
 
-    # # Next, a maxpooling layer is applied. (Use kernel_size=2 and stride=2.)
-    # x = self.maxpool(x) # output (8, 14, 14)
+    x = self.conv19(x) # output (1, 128, 128)
+    
+    return x
 
-    # # Next, a convolutional layer with 16 filters is applied. Then ReLU is applied.
-    # x = self.conv3(x) # output (16, 14, 14)
-    # x = self.relu(x)
+#%%
 
-    # # Next, a flattening layer is applied.
-    # x = torch.flatten(x, start_dim=1) # output (16*14*14)x1
+# Dimmentions (check)
 
-    # # Finally, a linear (sometimes called "dense") layer is applied.
-    # x = self.dense1(x) # output (10x1)
+model = UNet()
+test_input = torch.randn(16, 1, 128, 128)
+output = model(test_input)
+print(f'Output shape: {output.shape}')
 
-    return
+#%%
