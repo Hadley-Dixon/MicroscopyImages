731bb0179bbe6bc2ba7b5b39a2f4f08fb42109fc
Hadley Dixon <hadley.dixon22@gmail.com>
2024-05-12 17:19:24 -0700

    load

--- Final Project MATH 373.py
+++ Final Project MATH 373.py
@@ -9,3 +9,27 @@ Created on Sun May 12 13:55:33 2024
 @author: hadleydixon
 """
 
+#%%
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn as sk
+import sklearn.model_selection
+import torch
+import matplotlib.pyplot as plt
+
+npzfile_train = np.load('/Users/hadleydixon/Desktop/MicroscopyImages/counting-cells-in-microscopy-images-2024/train_data.npz')
+npzfile_test = np.load("/Users/hadleydixon/Desktop/MicroscopyImages/counting-cells-in-microscopy-images-2024/test_images.npz")
+df_train, df_val = sk.model_selection.train_test_split(npzfile_train, train_size=.8)
+
+X = npzfile_train['X']
+y = npzfile_train['y']
+
+# Image display (check)
+
+i = 0
+plt.imshow(X[i]/255 + .5*y[i], cmap='gray')
+
+#%%
+
