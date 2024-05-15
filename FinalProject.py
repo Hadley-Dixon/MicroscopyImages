6ce19a1a7462491c5d6e2c787323c4ed8e7fdfb2
Hadley Dixon <hadley.dixon22@gmail.com>
2024-05-14 18:30:02 -0700

    Final

--- FinalProject.py
+++ FinalProject.py
@@ -91,14 +91,11 @@ x_batch.shape
 
 #%%
 
-# U-Net Architecture --> move from collab
+# U-Net Architecture
 
 class UNet(torch.nn.Module):
   def __init__(self):
     super().__init__()
-
-    self.relu = torch.nn.ReLU()
-    self.maxpool = torch.nn.MaxPool2d(kernel_size=2, stride=2)
 
     # Encode
 
@@ -110,106 +107,127 @@ class UNet(torch.nn.Module):
     self.conv6 = torch.nn.Conv2d(256, 256, kernel_size = 3, padding ='same')
     self.conv7 = torch.nn.Conv2d(256, 512, kernel_size = 3, padding ='same')
     self.conv8 = torch.nn.Conv2d(512, 512, kernel_size = 3, padding ='same')
-    self.conv9 = torch.nn.Conv2d(512, 1024, kernel_size = 3, padding='same')
-    self.conv10 = torch.nn.Conv2d(1024, 1024, kernel_size = 3, padding='same')
+
+    # Base
+
+    self.conv9 = torch.nn.Conv2d(512, 1024, kernel_size = 3, padding ='same')
+    self.conv10 = torch.nn.Conv2d(1024, 1024, kernel_size = 3, padding ='same')
+    self.conv11 = torch.nn.Conv2d(1024, 512, kernel_size = 3, padding = 'same')
 
     # Decode
+    self.conv12 = torch.nn.Conv2d(1024, 512, kernel_size = 3, padding = 'same')
+    self.conv13 = torch.nn.Conv2d(512, 512, kernel_size = 3, padding = 'same')
+    self.conv14 = torch.nn.Conv2d(512, 256, kernel_size = 3, padding = 'same')
+    self.conv15 = torch.nn.Conv2d(512, 256, kernel_size = 3, padding = 'same')
+    self.conv16 = torch.nn.Conv2d(256, 256, kernel_size = 3, padding = 'same')
+    self.conv17 = torch.nn.Conv2d(256, 128, kernel_size = 3, padding = 'same')
+    self.conv18 = torch.nn.Conv2d(256, 128, kernel_size = 3, padding = 'same')
+    self.conv19 = torch.nn.Conv2d(128, 128, kernel_size = 3, padding = 'same')
+    self.conv20 = torch.nn.Conv2d(128, 64, kernel_size = 3, padding = 'same')
+    self.conv21 = torch.nn.Conv2d(128, 64, kernel_size = 3, padding = 'same')
+    self.conv22 = torch.nn.Conv2d(64, 64, kernel_size = 3, padding = 'same')
+    self.conv23 = torch.nn.Conv2d(64, 1, kernel_size = 3, padding = 'same')
 
-    self.upConv1 = torch.nn.ConvTranspose2d(1024, 512, kernel_size = 2, stride=2, padding = 0)
-    self.upConv2 = torch.nn.ConvTranspose2d(512, 256, kernel_size = 2, stride=2, padding = 0)
-    self.upConv3 = torch.nn.ConvTranspose2d(256, 128, kernel_size = 2, stride=2, padding = 0)
-    self.upConv4 = torch.nn.ConvTranspose2d(128, 64, kernel_size = 2, stride=2, padding = 0)
 
-    self.conv11 = torch.nn.Conv2d(1024, 512, kernel_size = 3, padding = 'same')
-    self.conv12 = torch.nn.Conv2d(512, 512, kernel_size = 3, padding = 'same')
-    self.conv13 = torch.nn.Conv2d(512, 256, kernel_size = 3, padding = 'same')
-    self.conv14 = torch.nn.Conv2d(256, 256, kernel_size = 3, padding = 'same')
-    self.conv15 = torch.nn.Conv2d(256, 128, kernel_size = 3, padding = 'same')
-    self.conv16 = torch.nn.Conv2d(128, 128, kernel_size = 3, padding = 'same')
-    self.conv17 = torch.nn.Conv2d(128, 64, kernel_size = 3, padding = 'same')
-    self.conv18 = torch.nn.Conv2d(64, 64, kernel_size = 3, padding = 'same')
-    self.conv19 = torch.nn.Conv2d(64, 1, kernel_size=3, padding='same')
+    # Study Session Notes: O'Connor doesn't prefer this method of up-convolution
+      # self.upConv1 = torch.nn.ConvTranspose2d(1024, 512, kernel_size = 2, stride = 2, padding = 0)
+      # self.upConv2 = torch.nn.ConvTranspose2d(512, 256, kernel_size = 2, stride = 2, padding = 0)
+      # self.upConv3 = torch.nn.ConvTranspose2d(256, 128, kernel_size = 2, stride = 2, padding = 0)
+      # self.upConv4 = torch.nn.ConvTranspose2d(128, 64, kernel_size = 2, stride = 2, padding = 0)
+
+    self.relu = torch.nn.ReLU()
+    self.maxpool = torch.nn.MaxPool2d(kernel_size = 2, stride = 2)
+    self.upSample = torch.nn.Upsample(scale_factor = 2)
 
   def forward(self, x):
 
     # Encode
 
-    x = self.conv1(x) # output (64, 128, 128)
-    x = self.relu(x)
+    x = self.conv1(x)
+    x = self.relu(x) # output (64, 128, 128)
     x = self.conv2(x) # output (64, 128, 128)
     x = self.relu(x)
 
     copy1 = x
-
     x = self.maxpool(x) # output (64, 64, 64)
 
-    x = self.conv3(x) # output (128, 64, 64)
-    x = self.relu(x)
-    x = self.conv4(x) # output (128, 64, 64)
-    x = self.relu(x)
+    x = self.conv3(x)
+    x = self.relu(x) # output (128, 64, 64)
+    x = self.conv4(x)
+    x = self.relu(x) # output (128, 64, 64)
 
     copy2 = x
-
     x = self.maxpool(x) # output (128, 32, 32)
 
-    x = self.conv5(x) # output (256, 32, 32)
-    x = self.relu(x)
+    x = self.conv5(x)
+    x = self.relu(x) # output (256, 32, 32)
     x = self.conv6(x) # output (256, 32, 32)
     x = self.relu(x)
 
     copy3 = x
-
     x = self.maxpool(x) # output (256, 16, 16)
 
-    x = self.conv7(x) # output (512, 16, 16)
-    x = self.relu(x)
-    x = self.conv8(x) # output (512, 16, 16)
-    x = self.relu(x)
+    x = self.conv7(x)
+    x = self.relu(x) # output (512, 16, 16)
+    x = self.conv8(x)
+    x = self.relu(x) # output (512, 16, 16)
 
     copy4 = x
-
     x = self.maxpool(x) # output (512, 8, 8)
+
+    # Base
 
     x = self.conv9(x) # output (1024, 8, 8)
     x = self.relu(x)
     x = self.conv10(x) # output (1024, 8, 8)
     x = self.relu(x)
 
-    # Decode
-
-    x = self.upConv1(x) # output (512, 16, 16)
-    x = torch.cat((copy4, x), dim = 1) # output (1024, 16, 16)
-
+    x = self.upSample(x) # output (1024, 16, 16)
     x = self.conv11(x) # output (512, 16, 16)
     x = self.relu(x)
-    x = self.conv12(x) # output (512, 16, 16)
+
+    # Decode
+    x = torch.cat((copy4, x), dim = 1) # output (1024, 16, 16)
+
+    x = self.conv12(x) # output (1024, 16, 16)
+    x = self.relu(x)
+    x = self.conv13(x) # output (512, 16, 16)
     x = self.relu(x)
 
-    x = self.upConv2(x) # output (256, 32, 32)
-    x = torch.cat((copy3, x), dim = 1) # output (512, 32, 32)
-
-    x = self.conv13(x) # output (256, 32, 32)
-    x = self.relu(x)
+    x = self.upSample(x) # output (512, 32, 32)
     x = self.conv14(x) # output (256, 32, 32)
     x = self.relu(x)
 
-    x = self.upConv3(x) # output (128, 64, 64)
+    x = torch.cat((copy3, x), dim = 1) # output (512, 32, 32)
+
+    x = self.conv15(x) # output (256, 32, 32)
+    x = self.relu(x)
+    x = self.conv16(x) # output (256, 32, 32)
+    x = self.relu(x)
+
+    x = self.upSample(x) # output (256, 64, 64)
+    x = self.conv17(x) # output (128, 64, 64)
+    x = self.relu(x)
+
     x = torch.cat((copy2, x), dim = 1) # output (256, 64, 64)
 
-    x = self.conv15(x) # output (128, 64, 64)
+    x = self.conv18(x) # output (128, 64, 64)
     x = self.relu(x)
-    x = self.conv16(x) # output (128, 64, 64)
+    x = self.conv19(x) # output (128, 64, 64)
     x = self.relu(x)
 
-    x = self.upConv4(x) # output (64, 128, 128)
+    x = self.upSample(x) # output (128, 128, 128)
+    x = self.conv20(x) # output (64, 128, 128)
+    x = self.relu(x)
+
     x = torch.cat((copy1, x), dim = 1) # output (128, 128, 128)
 
-    x = self.conv17(x) # output (64, 128, 128)
+    x = self.conv21(x) # output (64, 128, 128)
     x = self.relu(x)
-    x = self.conv18(x) # output (64, 128, 128)
+    x = self.conv22(x) # output (64, 128, 128)
     x = self.relu(x)
 
-    x = self.conv19(x) # output (1, 128, 128)
+    x = self.conv23(x) # output (1, 128, 128)
 
     return x
 
